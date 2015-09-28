89967bb1ba4495cacadd8216d5014d3f1f8a749a
willworks <scnuzhonghanjin@gmail.com>
2015-09-28 21:28:55 +0800

    node http模块笔记

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 // node http模块引入
-var http = require("http");
-var url = "http://www.bepotato.com/";
-var data = "";
+var http = require("http"),
+
+    // 访问请求配置
+    options = {  
+        host: 'www.bepotato.com',
+        path: '/',
+        method: 'GET',
+        headers: {
+            'Accept': 'text/html'
+        }
+    },
+    data = "";
 
 // 发起请求
-var req = http.request(url, function(res){
+// PS:node属于C/S通讯，不是B/S，故这种请求，可以跨域
+var req = http.request(options, function(res){
     // 设置显示编码
     res.setEncoding("utf8");
     // chunk指的是服务器数据的返回是一段一段的，这个和TCP滑动窗口协议有关，需要使用data串接起来，参照buffer
